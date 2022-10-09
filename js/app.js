0a3fef99e6c831a638326efd9b68dcf30dbe6a9d
Akbarov4620 <112079467+Akbarov4620@users.noreply.github.com>
2022-10-09 14:40:12 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+alert("Salom" );
+var ism = prompt("Ismingiz nima ?");
+console.log(ism);
+var familiya = prompt("Familiyangiz nima ?");
+console.log(familiya);
+var age = prompt("Yoshingiz nechchida ?");
+console.log(age);
+var uqish= prompt("Qayerda o'qiysiz ?");
+console.log(uqish);
+var kelajak= prompt("Kelajakda kim bo'lmoqchisiz ?");
+console.log(kelajak);
+alert("Savollarga javob berganingiz uchun raxmat !")
+console.log("Hello world");
